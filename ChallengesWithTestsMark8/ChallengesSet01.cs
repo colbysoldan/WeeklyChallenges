bd41c8fb7ab0592be1da7b90dc3da97ac6bcbaa7
Colby Soldan <colbylsoldan@gmail.com>
2022-06-13 14:55:24 -0500

    finished week 2

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,14 +6,16 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            //if(num1 == num2)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+
+            return num1 == num2;
         }
 
         public double Subtract(double minuend, double subtrahend)
@@ -38,14 +40,16 @@ namespace ChallengesWithTestsMark8
 
         public string GetGreeting(string nameOfPerson)
         {
-            if (nameOfPerson != "")
-            {
-                return $"Hello, {nameOfPerson}!";
-            }
-            else
-            {
-                return "Hello!";
-            }
+            //if (nameOfPerson != "")
+            //{
+            //    return $"Hello, {nameOfPerson}!";
+            //}
+            //else
+            //{
+            //    return "Hello!";
+            //}
+
+            return nameOfPerson != "" ? $"Hello, {nameOfPerson}!" : "Hello!";
         }
         public string GetHey()
         {
